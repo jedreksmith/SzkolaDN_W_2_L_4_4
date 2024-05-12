a3be9ec5b24b115875f18580807b09caceaffab3
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-05-12 21:54:50 +0200

    Add project files.

---
+++ Ex_w_2_l_4_4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex_w_2_l_4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Three types");
+
+            int a = 10;
+
+            string school = ("Szkoła Dotneta");
+
+            double b = 12.5;
+        }
+    }
+}
